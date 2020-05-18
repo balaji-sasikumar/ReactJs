05a9035949e7040c0fe19f66f631c916ee558302
deathstroke45 <balajisasi739030@gmail.com>
2020-05-19 00:48:00 +0530

    SPA-2

--- confusion/src/Components/DishdetailComponent.js
+++ confusion/src/Components/DishdetailComponent.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardTitle, CardBody } from "reactstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 
@@ -8,9 +12,19 @@ const DishDetail = (props) => {
     return (
         <div className="container">
             <div className="row">
+                <Breadcrumb>
 
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
                 <RenderDish dish={props.dish} />
-
+                <RenderComments comments={props.comments} />
             </div>
         </div>
     );
@@ -30,10 +44,7 @@ function RenderDish({ dish }) {
                         </CardBody>
                     </Card>
                 </div>
-                <div className="col-12 col-md-5 m-1" >
-                    <h4>Comments</h4>
-                    <RenderComments comments={dish.comments} />
-                </div>
+
             </React.Fragment>
         )
     }
@@ -49,8 +60,11 @@ function RenderComments({ comments }) {
 
             return (
                 <React.Fragment>
-                    <li>{co.comment}</li><br />
-                    <li>-- {co.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(co.date)))} </li><br />
+                    <div className="col-12 col-md-5 m-1" >
+                        <h4>Comments</h4>
+                        <li>{co.comment}</li><br />
+                        <li>-- {co.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(co.date)))} </li><br />
+                    </div>
                 </React.Fragment>
             )
 
