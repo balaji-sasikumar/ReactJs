a4056a9783e3c671bc3817be23fedffccb1822bb
deathstroke45 <balajisasi739030@gmail.com>
2020-05-18 21:02:55 +0530

    React Router

--- confusion/src/Components/MainComponent.js
+++ confusion/src/Components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponentwithCard';
-import Hello from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import DishDetail from './DishdetailComponent';
+import Home from './HomeComponent'
 import { DISHES } from '../shared/dishes';
+import {Switch,Route,Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,22 +13,18 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
-    return (
+    return (  
       <div>
         <Header />
-        {/* here the dishes updated */}
-        {/* <Hello dishes={this.state.dishes}/> */}
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> 
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home" component={()=> <Home />} />
+          <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} />   
+          <Redirect to="/home" /> 
+        </Switch>
         <Footer />
       </div>
     );
