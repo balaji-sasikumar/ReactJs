d2048fdd5acc76be359cd04b7900b3e78b8330e3
deathstroke45 <balajisasi739030@gmail.com>
2020-05-21 23:14:05 +0530

    Redux Thunk

--- confusion/src/Components/DishdetailComponent.js
+++ confusion/src/Components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 
@@ -47,7 +48,25 @@ function RenderComments({ comments, addComment, dishId }) {
 
 const Dish = props => {
     
-    if (props.dish) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
         return (
             <div className="container">
                 <div className="row">
@@ -70,7 +89,7 @@ const Dish = props => {
                 </div>
             </div>
         );
-    }
+    
     else {
         return (
             <div></div>
