a23d9154f4ffe2401a7787117564a9f657acf20e
deathstroke45 <balajisasi739030@gmail.com>
2020-06-12 22:08:34 +0530

    after 4 th assignment

--- confusion/src/Components/MenuComponent.js
+++ confusion/src/Components/MenuComponent.js
@@ -1,48 +1,67 @@
-import React from 'react'
-import { Media } from 'reactstrap'
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
-function Sample({dish}) {
+function RenderMenuItem({ dish, onClick }) {
     return (
-        <Media tag="li">
-            <Media left middle>
-                <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-                <Media heading>
-                    {dish.name}
-                </Media>
-                <p>
-                    {dish.description}
-                </p>
-            </Media>
-        </Media>
-
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
     );
 }
 
-
-const Hello=(props)=> {
-    const menu = props.dishes.map((dish) => {
+const Menu = props => {
+    const menu = props.dishes.dishes.map(dish => {
         return (
-            <div key={dish.id} className="col-12 mt-5">
-                <Sample dish={dish}/>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} />
+            </div>
+        )
+    });
 
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
             </div>
         );
-    });
-    return (
-        <div className="container">
-            <div className="row">
-                <Media list>
-                    {menu}
-                </Media>
+    }
+    else if (props.dishes.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
             </div>
-        </div>
-    );
-
-
+        );
+    }
+    else
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
 }
 
-
-export default Hello;
-
+export default Menu;
